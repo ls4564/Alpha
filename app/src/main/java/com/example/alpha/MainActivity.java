8cc71ac8d9ed5afbd9f0cb3ab218e664e6bc483b
Lior1254 <liorshemtov2008@gmail.com>
2025-10-28 15:52:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/alpha/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.alpha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menuAuth) {
+            Intent intent = new Intent(this, AuthActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menuListView) {
+            Intent intent = new Intent(this, ListViewActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
